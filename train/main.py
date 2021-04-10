36620823e967072a86f5a031fef2bdcf5b3d5ad0
Nicolas Aagnes <nicolas.aagnes@gmail.com>
2021-04-10 16:02:43 +0200

    add cli arguments

--- train/main.py
+++ train/main.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 
 import tensorflow as tf
@@ -8,8 +9,13 @@ os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data", help="Data directory.", required=True)
+    parser.add_argument("--outdir", help="Output directory.", required=True)
+    args = parser.parse_args()
+
     dataset_params = {
-        "directory": "data",
+        "directory": args.data,
         "image_size": (640, 640),
         "validation_split": 0.2,
         "seed": 123,
@@ -23,7 +29,7 @@ def main():
     print(model.summary())
 
     checkpoint = keras.callbacks.ModelCheckpoint(
-        filepath="checkpoints/checkpoint",
+        filepath=f"{args.outdir}/checkpoint",
         monitor="val_sparse_categorical_accuracy",
         save_weights_only=True,
         save_best_only=True,
